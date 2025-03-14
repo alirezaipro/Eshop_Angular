366875496692e7845b6c5aba9b4efad23cb4383b
ali rezaei <alirezai.pro@gmail.com>
2025-03-14 16:15:41 +0330

    feat: add login component and service

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {LoginRequestDto} from '../../dtos/account/login.dto';
+import {AccountService} from '../../services/account.service';
+import {ToastrService} from 'ngx-toastr';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  model: LoginRequestDto = {
+    email: '',
+    password: ''
+  };
+
+  constructor(private accountService: AccountService,
+              private toastrService: ToastrService,
+              private router: Router) {
+  }
+
+  onLoginUser() {
+    this.accountService.login(this.model).subscribe(response => {
+      if (response.isSuccess) {
+
+        localStorage.setItem('jwt_token', response.data);
+        this.toastrService.success(response.message, 'موفق');
+
+        setTimeout(() => {
+          this.router.navigate(['/']);
+        }, 3000);
+
+      } else {
+        this.toastrService.error(response.message, 'خطا');
+      }
+    })
+  }
 
 }
--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -1,10 +1,10 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {LoginDto} from '../dtos/account/login.dto';
 import {RegisterRequestDto, RegisterResponseDto} from '../dtos/account/register.dto';
 import {ApiAddress} from '../utilities/api-address.utility';
 import {ApiResponse} from '../dtos/common/api-response';
 import {Observable} from 'rxjs';
+import {LoginRequestDto} from '../dtos/account/login.dto';
 
 @Injectable({
   providedIn: 'root'
@@ -14,11 +14,11 @@ export class AccountService {
   constructor(private httpClient: HttpClient) {
   }
 
-  login(model: LoginDto) {
-
+  login(model: LoginRequestDto):Observable<ApiResponse<string>> {
+    return this.httpClient.post<ApiResponse<string>>(ApiAddress.login, model);
   }
 
-  register(model: RegisterRequestDto):Observable<ApiResponse<RegisterResponseDto>> {
+  register(model: RegisterRequestDto): Observable<ApiResponse<RegisterResponseDto>> {
 
     return this.httpClient.post<ApiResponse<RegisterResponseDto>>(ApiAddress.register, model);
 
